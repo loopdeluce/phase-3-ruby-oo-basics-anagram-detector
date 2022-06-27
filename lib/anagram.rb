6299ee87945b1afbb5382e6acab5d58164c1aa76
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-27 17:48:58 -0600

    commit tree 458931832b7e05e03ba5dd74fd164c9a5f561c72

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagramArray)
+    anagramArray.filter do |potentialAnagram|
+      wordChars = @word.chars
+      potentialAnagramChars = potentialAnagram.chars
+      wordChars.sort == potentialAnagramChars.sort
+    end
+  end
+end
